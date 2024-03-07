ae9e916b11147d9976a29fb94a2cdff5e2c01482
Alexis Saettler <alexis@saettler.org>
2024-03-07 21:06:02 +0100

    add middleware test

--- tests/Middleware/TrackPlausiblePageviewsTest.php
+++ tests/Middleware/TrackPlausiblePageviewsTest.php
@@ -5,12 +5,33 @@ namespace VincentBean\Plausible\Tests\Unit;
 use Illuminate\Http\Client\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Route;
 use VincentBean\Plausible\Middleware\TrackPlausiblePageviews;
 use VincentBean\Plausible\Tests\TestCase;
 
 class TrackPlausiblePageviewsTest extends TestCase
 {
     public function test_it_sends_event(): void
+    {
+        $post_url = 'http://plausible-domain.test/api/event';
+
+        Http::fake([$post_url => Http::response()]);
+
+        Route::middleware(TrackPlausiblePageviews::class)
+            ->get('/', fn () => 'Hello World');
+
+        $this->get('/');
+
+        Http::assertSent(function (Request $request) use ($post_url) {
+            return $request->hasHeader('X-Forwarded-For') &&
+                $request->hasHeader('user-agent') &&
+                $request->url() === $post_url &&
+                $request['name'] === 'pageview' &&
+                $request['domain'] === 'plausible-tracking-domain.test';
+        });
+    }
+
+    public function test_it_handles_event(): void
     {
         $post_url = 'http://plausible-domain.test/api/event';
 
